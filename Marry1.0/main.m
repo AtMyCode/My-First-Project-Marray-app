10ed02848901e4743ead0e4d29d036b440ac83ea
apple <apple@appledeiMac.local>
2014-09-28 10:35:52 +0000

    Initial Commit

---
+++ Marry1.0/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Marry1.0
+//
+//  Created by apple on 14-9-28.
+//  Copyright (c) 2014年 lj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Marry1.0Tests/Marry1_0Tests.m
@@ -0,0 +1,34 @@
+//
+//  Marry1_0Tests.m
+//  Marry1.0Tests
+//
+//  Created by apple on 14-9-28.
+//  Copyright (c) 2014年 lj. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Marry1_0Tests : XCTestCase
+
+@end
+
+@implementation Marry1_0Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
